86ecf4c6f373f75288173b685e639188ec2d213a
Monika kumari <monikakumari4687@gmail.com>
2026-08-06 11:58:06 +0530

    DAY11

--- LEC-11/index.js
+++ LEC-11/index.js
@@ -1,5 +1,6 @@
 function outer(){
     let a = 10;
+    // block scope variable a is created and initialized to 10
     function inner (){
         a++;
         console.log(a);
@@ -7,6 +8,13 @@ function outer(){
     // inner(); // This line is commented out, so inner() is not called here
      return inner;
 }
-let f1 = outer();
+
+// closure  har baar naya banta hai 
+let f1 = outer(); 
+f1()
+f1()
+let f2 = outer();
 console.log(f1); // This will log the inner function definition
-f1(); // This will call the inner function, which increments 'a' and logs it
+f1(); 
+f2(); 
+f2();// This will call the inner function, which increments 'a' and logs it
